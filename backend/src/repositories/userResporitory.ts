2b1f8127e4f168418bbf4d6f12517835ca7fc1a3
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2025-09-27 13:55:27 -0300

    recomeçando do jeito certo

--- backend/src/repositories/userResporitory.ts
+++
@@ -1,6 +0,0 @@
-import pkg from "pg";
-import { UserEntity } from "../entities/userEntity";
-
-export interface IUserRepository {
-    Save(user: UserEntity): Promise<pkg.QueryResult<any>>;
-}
--- backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-import { config } from "dotenv";
-config();
-
-const PORT = parseInt(process.env.PORT ?? "3333");
-
-app.listen(PORT, "0.0.0.0", () => console.log(`rodando na porta ${PORT}`));
