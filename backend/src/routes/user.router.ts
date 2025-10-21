f8d700ffc0d6410838464353a1cf641cfcd5b4c6
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2025-10-21 10:33:22 -0300

    sistema de validação do userEntity

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,8 +1,13 @@
 import express, { type Router, type Request, type Response } from "express";
+import createUserEntity from "../entities/user.entity";
 
 const USER_ROUTER: Router = express.Router();
 
 USER_ROUTER.get("/", (_req: Request, res: Response) => {
+
+    let user = createUserEntity();
+    console.log(user.isValidFields() ? "sim" : "não");
+
     res.status(200).json({
         "sucess": true,
         "message": "USER system is functional"
