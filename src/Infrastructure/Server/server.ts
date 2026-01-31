33883099a0e8ad6bd7a0a5b2148e8aef76eaa013
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-01-31 19:21:04 -0300

    updated name for import UserRouter

--- src/Infrastructure/Server/server.ts
+++ src/Infrastructure/Server/server.ts
@@ -2,11 +2,11 @@ import AppServer from "@Infrastructure/Server/app";
 import env from "@Infrastructure/Config/env";
 import APIRouter from "@Infrastructure/Server/router";
 
-import UserRouter from "@Adapters/in/Users/UserController"
+import UserRouter from "@Adapters/in/User/UserController"
 
 const ApiRouter: APIRouter = new APIRouter();
 ApiRouter.loadRoute("/user", UserRouter);
 
-const App: AppServer = new AppServer(env.HOST, env.PORT, ApiRouter);
+const App: AppServer = new AppServer(env.SERVER_HOST, env.SERVER_PORT, ApiRouter);
 
 App.run();
