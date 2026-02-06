b99559a0a0b78e89ea92108da579cfeb8cf0c230
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-02-06 09:17:26 -0300

    feat: APIError for normalize errors

---
+++ src/Infrastructure/APIUtils/APIError.ts
@@ -0,0 +1,8 @@
+export default class APIError extends Error {
+    constructor(
+        public readonly message: string,
+        public readonly statusCode: number
+    ) {
+        super(message);
+    }
+}
